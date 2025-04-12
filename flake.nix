513839b5222d309507edb1f0433ea87b5ff75adf
Connor Rhodes <connor@rhodes.contact>
2025-04-12 16:36:03 -0500

    add initial flake

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+  outputs = { self, nixpkgs, ... }: {
+    nixConfigurations.testvm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
