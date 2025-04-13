de398052b8047bd56c4510ecd19cf65b05519143
Connor Rhodes <connor@rhodes.contact>
2025-04-12 21:36:32 -0500

    add common packages, latitude server

--- flake.nix
+++ flake.nix
@@ -3,12 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }: {
+
+    nixosConfigurations.latitude = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./hosts/nixos/latitude/configuration.nix ];
+    };
+
     nixosConfigurations.testvm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./hosts/nixos/testvm/configuration.nix ];
     };
+
   };
 }
--- hosts/nixos/testvm/configuration.nix
+++ hosts/nixos/testvm/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ../../common/packages.nix
     ];
 
   # Bootloader.
