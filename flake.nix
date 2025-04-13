352bdde748dfd47208cb699ff7aed62aee15e03a
Connor Rhodes <connor@rhodes.contact>
2025-04-12 21:22:50 -0500

    update style, add description

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
+  description = "Nix config for personal infrastructure";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
+
   outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.testvm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ 
-        ./hosts/nixos/testvm/configuration.nix
-      ];
+      modules = [ ./hosts/nixos/testvm/configuration.nix ];
     };
   };
 }
