534f2ebb3e03b7bb2667719b3a463051ad01eb71
Connor Rhodes <connor@rhodes.contact>
2025-04-13 21:21:19 -0500

    add home-manager as input

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs"; # ensure package versions are the same as nixpkgs (eg. 24.11)
+    };
   };
 
   outputs = { ... }@inputs: with inputs;
