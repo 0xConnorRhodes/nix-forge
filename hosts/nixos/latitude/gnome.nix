113d329206ab464b7533dcd346e4a820089c9b9e
Connor Rhodes <connor@rhodes.contact>
2025-04-13 19:37:05 -0500

    break latitude gnome config into module

---
+++ hosts/nixos/latitude/gnome.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+let
+  user = "connor";
+  enable_autologin = false;
+in
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = enable_autologin;
+  services.displayManager.autoLogin.user = user;
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = enable_autologin;
+  systemd.services."autovt@tty1".enable = enable_autologin;
+
+  environment.systemPackages = with pkgs; [
+    #gsettings-desktop-schemas
+    dconf2nix
+  ];
+}
