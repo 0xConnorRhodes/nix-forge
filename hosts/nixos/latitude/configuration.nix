38266a6b34a3b04b42137d4b1a1151b27f28e79a
Connor Rhodes <connor@rhodes.contact>
2025-04-13 21:04:50 -0500

    prune redundant options in latitude

--- hosts/nixos/latitude/configuration.nix
+++ hosts/nixos/latitude/configuration.nix
@@ -17,7 +17,6 @@
   };
 
   config = {
-    # Bootloader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
@@ -52,28 +51,14 @@
     # Enable CUPS to print documents.
     services.printing.enable = true;
 
-    # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.${config.myConfig.username} = {
       isNormalUser = true;
       description = "Connor Rhodes";
       extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [
-      #  thunderbird
-      ];
     };
 
     # Install firefox.
     programs.firefox.enable = true;
-
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
-
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-    ];
 
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
@@ -94,12 +79,6 @@
     # Or disable the firewall altogether.
     # networking.firewall.enable = false;
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.11"; # Did you read the comment?
+    system.stateVersion = "24.11";
   };
 }
