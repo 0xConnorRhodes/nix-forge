913dc42423b7f468e6bb8b01dba34f03ce450852
Connor Rhodes <connor@rhodes.contact>
2025-04-13 19:37:05 -0500

    break latitude gnome config into module

--- hosts/nixos/latitude/configuration.nix
+++ hosts/nixos/latitude/configuration.nix
@@ -3,11 +3,14 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-
+let
+  user = "connor";
+in
 {
   imports =
     [
       ./hardware-configuration.nix
+      ./gnome.nix
       ../../common/nixos-common.nix
       ../../common/packages.nix
       ../../../modules/incus.nix
@@ -45,43 +48,11 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.connor = {
+  users.users.${user} = {
     isNormalUser = true;
     description = "Connor Rhodes";
     extraGroups = [ "networkmanager" "wheel" ];
