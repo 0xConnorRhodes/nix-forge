23b02a2279e44e2228b840e02c894b9c06ffad07
Connor Rhodes <connor@rhodes.contact>
2025-04-18 17:36:25 -0500

    move pkgs into latitude config

--- hosts/nixos/latitude/configuration.nix
+++ hosts/nixos/latitude/configuration.nix
@@ -7,7 +7,7 @@
       ./gnome.nix
       #./kiosk.nix
       ../../common/nixos-common.nix
-      ../../common/packages.nix
+     #../../common/packages.nix
       ../../../modules/nixos/incus.nix
       inputs.home-manager.nixosModules.default
     ];
@@ -22,35 +22,11 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
-    networking.hostName = "latitude"; # Define your hostname.
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-    # Enable networking
+    networking.hostName = "latitude";
     networking.networkmanager.enable = true;
 
-    # Set your time zone.
     time.timeZone = "America/Chicago";
 
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-
-    # Enable CUPS to print documents.
     services.printing.enable = true;
 
     users.users.${config.myConfig.username} = {
@@ -66,30 +42,34 @@
     ]; 
   };
 
-    # Install firefox.
     programs.firefox.enable = true;
-
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
 
     environment.systemPackages = with pkgs; [
       rpi-imager
+      git
+      neovim
+      chezmoi
+      zsh
+      fish
+      zoxide
+      fzf
+      fd
+      bat
+      ripgrep
+      just
+      tree
+      zip
+      unzip
+      htop
+      screen
+      lf
+      magic-wormhole
+      powershell
+      dua
+      mosh
+      mediainfo
+      aria2
+      nh
     ];
 
     system.stateVersion = "24.11";
