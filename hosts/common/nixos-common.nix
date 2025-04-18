e29424a0a96788de73ce73515c77dd3826ee8d2a
Connor Rhodes <connor@rhodes.contact>
2025-04-18 07:19:10 -0500

    add sudo config with dynamic username per host

--- hosts/common/nixos-common.nix
+++ hosts/common/nixos-common.nix
@@ -1,5 +1,17 @@
-{ inputs, ... }:
-
+{ inputs, config, ... }:
+let
+  username = config.myConfig.username;
+in
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  security.sudo.extraRules = [
+    { users = [ username ];
+      commands = [
+        { command = "ALL" ;
+	  options = [ "NOPASSWD" ];
+	}
+      ];
+    }
+  ];
 }
