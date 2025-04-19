ed3970190979abcaee84bc8672745ff12e4444d7
Connor Rhodes <connor@rhodes.contact>
2025-04-19 10:08:54 -0500

    add gnome module for mpro

---
+++ hosts/nixos/mpro/gnome.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }:
+let
+  username = config.myConfig.username;
+  enable_autologin = true;
+in
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = enable_autologin;
+  services.displayManager.autoLogin.user = username;
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = enable_autologin;
+  systemd.services."autovt@tty1".enable = enable_autologin;
+
+  environment.systemPackages = with pkgs; [
+    #gsettings-desktop-schemas
+    dconf2nix
+    gnome-tweaks
+    gnome-themes-extra # provides adwaita dark theme in gnome tweaks for legacy applications
+  ];
+
+  # remote desktop
+  services.gnome.gnome-remote-desktop.enable = true;
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+  services.xrdp.openFirewall = true;
+}
