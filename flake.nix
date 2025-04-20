6027c65a846a9760a6c281ae162dda331329064d
Connor Rhodes <connor@rhodes.contact>
2025-04-20 12:55:05 -0500

    update whitespace

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
@@ -14,9 +15,6 @@
       url = "github:nix-community/nix-index-database"; 
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
