5e578b6e9e7c8b48e174b2f5f31caa01fae7d3be
Connor Rhodes <connor@rhodes.contact>
2025-04-24 09:02:13 -0500

    add font management to darwin

--- hosts/darwin/traveller/darwin-config.nix
+++ hosts/darwin/traveller/darwin-config.nix
@@ -3,7 +3,11 @@
 {
   system.defaults.finder = {
     AppleShowAllExtensions = true; # show file extensions in finder
-    _FXShowPosixPathInTitle = true;
+    _FXShowPosixPathInTitle = true; # show full path in finder title
+  };
+
+  system.defaults.dock = {
+    autohide = true;
   };
 
   system.keyboard = {
@@ -15,6 +19,13 @@
     InitialKeyRepeat = 10;
     KeyRepeat = 3;
   };
+
+  fonts.packages = [ (pkgs.nerdfonts.override { fonts = [
+      # pass keys from https://github.com/NixOS/nixpkgs/blob/nixos-24.11/pkgs/data/fonts/nerdfonts/shas.nix
+      # as strings to selectively install nerd fonts
+      "GeistMono"
+      ]; }) 
+  ];
 
   nix.extraOptions = ''
     experimental-features = nix-command flakes
