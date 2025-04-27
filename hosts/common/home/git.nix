72b84e5ddfea5380f9701b4ea397cdc27976b48c
Connor Rhodes <connor@rhodes.contact>
2025-04-27 14:11:03 -0500

    update git config

--- hosts/common/home/git.nix
+++ hosts/common/home/git.nix
@@ -5,14 +5,16 @@
     enable = true;
     userName = "Connor Rhodes";
     userEmail = "connor@rhodes.contact";
-
     ignores = [
       ".DS_Store"
     ];
-
     extraConfig = {
       push = {
         default = "current";
+        autoSetupRemote = true;
+      };
+      init = {
+        defaultBranch = "main";
       };
     };
   };
