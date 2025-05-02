c7dca5215f24ad8be79f46ebc95e9e67bf0169ff
Connor Rhodes <connor@rhodes.contact>
2025-05-01 20:46:27 -0500

    bash manual ghostty integration for subshells

--- hosts/common/home/bash.nix
+++ hosts/common/home/bash.nix
@@ -7,6 +7,13 @@ in
   programs.bash = {
     enable = true;
     shellAliases = shellAliases.myAliases;
+    # interactive shell config
+    initExtra = ''
+      # Ghostty shell integration for Bash. This should be at the top of your bashrc!
+      if [ -n "''${GHOSTTY_RESOURCES_DIR}" ]; then
+          builtin source "''${GHOSTTY_RESOURCES_DIR}/shell-integration/bash/ghostty.bash"
+      fi
+    '';
   };
 
   programs.fzf = {
--- hosts/common/home/ghostty.nix
+++ hosts/common/home/ghostty.nix
@@ -4,7 +4,6 @@
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
-    enableBashIntegration = true;
     enableFishIntegration = true;
   };
 
