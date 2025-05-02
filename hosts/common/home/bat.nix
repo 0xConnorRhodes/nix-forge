b6b0d41f594f29ef53a0ad06662749fc6fe597ee
Connor Rhodes <connor@rhodes.contact>
2025-05-01 21:29:01 -0500

    add bat config

---
+++ hosts/common/home/bat.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  programs.bat.enable = true;
+
+  programs.bat.config = {
+    # find available languages with `bat --list-languages`
+    map-syntax = [
+      "flake.lock:Nix"
+      "Gemfile*:Ruby"
+    ];
+  };
+}
--- hosts/common/packages.nix
+++ hosts/common/packages.nix
@@ -7,7 +7,6 @@ with pkgs; [
   fish
   fzf
   fd
-  bat
   just
   screen
   magic-wormhole
