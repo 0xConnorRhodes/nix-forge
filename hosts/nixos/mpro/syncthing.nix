a22162098f3ff967f920d4feaad27f3317042a49
Connor Rhodes <connor@rhodes.contact>
2025-05-04 14:10:35 -0500

    add syncthing shares

--- hosts/nixos/mpro/syncthing.nix
+++ hosts/nixos/mpro/syncthing.nix
@@ -1,17 +1,40 @@
 { pkgs, config, ... }:
 let
+  secrets = builtins.fromJSON (builtins.readFile ../../../secrets.json);
   guiPort = 42631;
 in
 {
   services.syncthing = {
     enable = true;
+    user = config.myConfig.username;
+    dataDir = "${config.myConfig.homeDir}/.local/syncthing";
     openDefaultPorts = true;
     key = "${config.myConfig.homeDir}/code/nix-forge/hosts/nixos/mpro/config/syncthing-key.pem";
     cert = "${config.myConfig.homeDir}/code/nix-forge/hosts/nixos/mpro/config/syncthing-cert.pem";
     guiAddress = "${config.myConfig.tailscaleIp}:${toString guiPort}";
-    settings.gui = {
-      user = "";
-      password = "";
+    settings = {
+      gui = { 
+        # disable built-in auth
+        user = ""; 
+        password = ""; 
+      };
+      devices = {
+        "phone" = { id = secrets.syncthing.phone_id; };
+      };
+      folders = {
+        "Audiobooks" = {
+          path = "/scary/phone/audiobooks"; # phone_path: internal/Music/Audiobooks
+          devices = [ "phone" ];
+        };
+        "Music Library" = {
+          path = "/scary/phone/music_library"; # phone_path: internal/Music/music_library
+          devices = [ "phone" ];
+        };
+        "Ebooks" = {
+          path = "/scary/phone/ebooks"; # phone_path: internal/Books
+          devices = [ "phone" ];
+        };
+      };
     };
   };
   systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder
