47c57d2d3fa446527152cc6792bcff19ec254547
Connor Rhodes <connor@rhodes.contact>
2025-05-12 19:21:59 -0500

    update wezterm desktop file

--- hosts/common/home/wezterm.nix
+++ hosts/common/home/wezterm.nix
@@ -6,4 +6,16 @@
     enableZshIntegration = true;
     enableBashIntegration = true;
   };
+  
+  xdg.desktopEntries = {
+    "org.wezfurlong.wezterm" = {
+      name = "WezTerm";
+      icon = "org.wezfurlong.wezterm";
+      type = "Application";
+      genericName = "Terminal";
+      exec = "env -u WAYLAND_DISPLAY wezterm";
+      terminal = false;
+      categories= ["System" "TerminalEmulator" "Utility"];
+    };
+  };
 }
