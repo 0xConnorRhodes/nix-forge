2429451ca8c565ad50d42650c7ecb5853ad6dda3
Connor Rhodes <connor@rhodes.contact>
2025-05-13 21:03:56 -0500

    add mpro jbod

--- hosts/nixos/mpro/mounts.nix
+++ hosts/nixos/mpro/mounts.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   fileSystems."/scary" = {
@@ -30,4 +30,29 @@
     fsType = "nfs4";
     options = [ "rw" "x-systemd.automount" "noauto" ];
   };
+
+  environment.systemPackages = with pkgs; [
+    mergerfs
+  ];
+
+  fileSystems."/mnt/jbod" = {
+    depends = [
+      # The `disk*` mounts have to be mounted in this given order.
+      "/mnt/disks/disk01"
+      "/mnt/disks/disk02"
+      ];
+      device = "/mnt/disks/disk*";
+      fsType = "mergerfs";
+      options = ["defaults" "fsname=mergerfs-jbod"];
+    };
+
+  fileSystems."/mnt/disks/disk01" = {
+    device = "/dev/disk/by-uuid/46e69ff6-a3c4-4862-a825-884682d27543";
+      fsType = "ext4";
+    };
+
+  fileSystems."/mnt/disks/disk02" = {
+    device = "/dev/disk/by-uuid/99b0b4c4-8386-423f-a01f-c2bd903c155b";
+      fsType = "ext4";
+    };
 }
