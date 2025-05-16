f130c01c95aa9dfe7c20a65c8ce71b8b1f9428ea
Connor Rhodes <connor@rhodes.contact>
2025-05-16 07:46:45 -0500

    refactor traveller packages

--- hosts/darwin/traveller/home.nix
+++ hosts/darwin/traveller/home.nix
@@ -1,19 +1,8 @@
 { config, pkgs, inputs, secrets, ... }:
-let
-  pkgsUnstable = import inputs.nixpkgs-unstable {
-    inherit (pkgs.stdenv.hostPlatform) system;
-    inherit (config.nixpkgs) config;
-  };
 
-  ruby = pkgs.ruby_3_4;
-  httparty = pkgs.bundlerEnv {
-    name = "httparty";
-    ruby = ruby;
-    gemdir = ../../../pkgs/ruby/httparty;
-  };
-in
 {
   imports = [
+    ./packages.nix
     ./comma.nix
     ./home-secret.nix
     ../../common/home/git.nix
@@ -69,73 +58,11 @@ in
     initExtra = import ../../common/home/posixFunctions.nix;
   };
 
-  home.packages = with pkgs; [
-    zoxide
-    powershell
-    mediainfo
-    aria2
-    tealdeer
-    htop
-    gotop
-    # rclone
-    # android-tools # provides adb
-    pkgsUnstable.bluesnooze # not in 24.11, must launch once and choose "launch at login" from menu bar icon
-    raycast
-    scrcpy
-    wezterm
-    #imagemagickBig # includes ghostscript
-    poppler_utils # provides pdftoppm file.pdf -png
-    minikube
-    kubectl
-
-    # mac-specific
-    blueutil
-    ffmpeg-full
-    iperf
-    darwin.iproute2mac
-    lazydocker
-    cloudflared
-    podman
-    podman-compose
-    unixtools.watch
-    unixtools.ping
-    emacs-macport
-    lima
-    mpv
-    
-    # GUI
-    utm
-    rectangle
-    neovide
-
-    # LATER
-    # lua
-    # mpv
-    # nodejs
-    # tesseract
-
-    # python packages
-    (python3.withPackages (python-pkgs: with python-pkgs; [
-      requests
-      jinja2
-      ipython
-    ]))
-
-    # ruby packages
-    (ruby.withPackages (ruby-pkgs: with ruby-pkgs; [
-      pry
-      dotenv
-      highline
-      httparty # local package
-    ]))
-  ] ++ (import ../../common/packages.nix { pkgs = pkgs; });
-
   xdg.configFile = {
     # "skhd/skhdrc".source = ./config/skhdrc;
     "ghostty/config".source = ./config/ghostty;
     "screen/screenrc".source = ../../common/home/config/screenrc;
   };
-
 
   # Don't show the "Last login" message for every new terminal.
   home.file.".hushlogin" = { text = ""; };
---
+++ hosts/darwin/traveller/packages.nix
@@ -0,0 +1,78 @@
+{ config, pkgs, inputs, secrets, ... }:
+let
+  pkgsUnstable = import inputs.nixpkgs-unstable {
+    inherit (pkgs.stdenv.hostPlatform) system;
+    inherit (config.nixpkgs) config;
+  };
+
+  ruby = pkgs.ruby_3_4;
+  httparty = pkgs.bundlerEnv {
+    name = "httparty";
+    ruby = ruby;
+    gemdir = ../../../pkgs/ruby/httparty;
+  };
+in
+{
+  home.packages = with pkgs; [
+    zoxide
+    powershell
+    mediainfo
+    aria2
+    tealdeer
+    htop
+    gotop
+    # rclone
+    # android-tools # provides adb
+    pkgsUnstable.bluesnooze # not in 24.11, must launch once and choose "launch at login" from menu bar icon
+    raycast
+    scrcpy
+    wezterm
+    #imagemagickBig # includes ghostscript
+    poppler_utils # provides pdftoppm file.pdf -png
+    minikube
+    kubectl
+    gcc # needed for neovim plugins
+
+    # mac-specific
+    blueutil
+    ffmpeg-full
+    iperf
+    darwin.iproute2mac
+    lazydocker
+    cloudflared
+    podman
+    podman-compose
+    unixtools.watch
+    unixtools.ping
+    emacs-macport
+    lima
+    mpv
+    
+    # GUI
+    utm
+    rectangle
+    neovide
+
+    # LATER
+    # lua
+    # mpv
+    # nodejs
+    # tesseract
+
+    # python packages
+    (python3.withPackages (python-pkgs: with python-pkgs; [
+      requests
+      jinja2
+      ipython
+    ]))
+
+    # ruby packages
+    (ruby.withPackages (ruby-pkgs: with ruby-pkgs; [
+      pry
+      dotenv
+      highline
+      httparty # local package
+    ]))
+  ] 
+  ++ (import ../../common/packages.nix { pkgs = pkgs; });
+}
