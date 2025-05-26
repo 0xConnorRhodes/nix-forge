ea3b3cf82e9a0aea22e6ad29089d74aca1d7a838
Connor Rhodes <connor@rhodes.contact>
2025-05-26 11:33:01 -0500

    update structure

--- modules/home/vscode/extensions-settings.nix
+++ modules/home/vscode/extensions-settings.nix
@@ -1,38 +1,36 @@
 { config, pkgs, inputs, ... }:
 
 {
-  programs.vscode = {
-    profiles.default = {
-      enableExtensionUpdateCheck = false;
-      userSettings = {
-        extensions.autoCheckUpdates = false;
+  programs.vscode.profiles.default = {
+    enableExtensionUpdateCheck = false;
+    userSettings.extensions.autoCheckUpdates = false;
+    # per-extension settings (comment shows extension ID)
+    userSettings = {
+      # vscodevim.vim
+      vim = {
+        leader = "<Space";
+        hlsearch = true;
+        useCtrlKeys = true;
+        useSystemClipboard = true;
+        foldfix = true; # don't unfold as you j/k through a folded heading
+      };
 
-        # vscodevim.vim
-        vim = {
-          leader = "<Space";
-          hlsearch = true;
-          useCtrlKeys = true;
-          useSystemClipboard = true;
-          foldfix = true; # don't unfold as you j/k through a folded heading
-        };
+      # redhat.vscode-yaml
+      redhat.telemetry.enabled = true; # YAML extension
 
-        # redhat.vscode-yaml
-        redhat.telemetry.enabled = true; # YAML extension
+      # sumneko.lua
+      Lua = {
+        window.statusBar = false;
+      };
 
-        # sumneko.lua
-        Lua = {
-          window.statusBar = false;
-        };
+      # yzhang.markdown-all-in-one
+      markdown.extension = {
+        math.enabled = false;
+      };
 
-        # yzhang.markdown-all-in-one
-        markdown.extension = {
-          math.enabled = false;
-        };
-
-        # satokaz.vscode-markdown-header-coloring
-        markdown-header-coloring = {
-          backgroundColor = false;
-        };
+      # satokaz.vscode-markdown-header-coloring
+      markdown-header-coloring = {
+        backgroundColor = false;
       };
     };
   };
