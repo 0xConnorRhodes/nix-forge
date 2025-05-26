f3fadd8c0ef1a5ac2273aef6a93b4c0b1a5d1c2a
Connor Rhodes <connor@rhodes.contact>
2025-05-26 10:57:47 -0500

    port vscode extension settings

--- modules/home/vscode/extensions.nix
+++ modules/home/vscode/extensions.nix
@@ -5,6 +5,32 @@
     profiles.default = {
       userSettings = {
         extensions.autoCheckUpdates = false;
+        # vscodevim.vim
+        vim = {
+          leader = "<Space";
+          hlsearch = true;
+          useCtrlKeys = true;
+          useSystemClipboard = true;
+          foldfix = true; # don't unfold as you j/k through a folded heading
+        };
+
+        # redhat.vscode-yaml
+        redhat.telemetry.enabled = true; # YAML extension
+
+        # sumneko.lua
+        Lua = {
+          window.statusBar = false;
+        };
+
+        # yzhang.markdown-all-in-one
+        markdown.extension = {
+          math.enabled = false;
+        };
+
+        # satokaz.vscode-markdown-header-coloring
+        markdown-header-coloring = {
+          backgroundColor = false;
+        };
       };
       enableExtensionUpdateCheck = false;
     };
