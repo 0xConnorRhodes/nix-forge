cca7ec764f106d5065ddf5a29466fcac77285b80
Connor Rhodes <connor@rhodes.contact>
2025-06-12 21:51:16 -0500

    update psd to pin newer commit and copy zen config to share/psd/browsers

--- modules/nixos/profile-sync-daemon.nix
+++ modules/nixos/profile-sync-daemon.nix
@@ -1,15 +1,44 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    (pkgs.profile-sync-daemon.overrideAttrs {
-        src = pkgs.fetchFromGitHub {
-          owner = "graysky2";
-          repo = "profile-sync-daemon";
-          rev = "cd8c2a37f152bd2bde167a0e066085ac23bc17d9";
-          hash = "sha256-+4VHOJryoNodJvx5Ug2TX7/T3OsFW5VwxaL9WUcp8xA=";
-        };
-    })
+  environment.systemPackages = with pkgs; [
+    (profile-sync-daemon.overrideAttrs (old: {
+
+      src = pkgs.fetchFromGitHub {
+        owner = "graysky2";
+        repo = "profile-sync-daemon";
+        rev = "cd8c2a37f152bd2bde167a0e066085ac23bc17d9";
+        hash = "sha256-+4VHOJryoNodJvx5Ug2TX7/T3OsFW5VwxaL9WUcp8xA=";
+      };
+
+      installPhase = (old.installPhase or "") + ''
+        cp $out/share/psd/contrib/zen $out/share/psd/browsers/zen
+      '';
+
+      # postInstall = (old.postInstall or "") + ''
+      #   # create zen file in share/psd/browsers
+      #   mkdir -p $out/share/psd/browsers
+      #   cat > $out/share/psd/browsers/zen <<'EOF'
+      #   if [[ -d "$HOME"/.zen ]]; then
+      #       index=0
+      #       PSNAME="$browser"
+      #       while read -r profileItem; do
+      #           if [[ $(echo "$profileItem" | cut -c1) = "/" ]]; then
+      #               # path is not relative
+      #               DIRArr[$index]="$profileItem"
+      #           else
+      #               # we need to append the default path to give a
+      #               # fully qualified path
+      #               DIRArr[$index]="$HOME/.zen/$profileItem"
+      #           fi
+      #           (( index=index+1 ))
+      #       done < <(grep '[Pp]'ath= "$HOME"/.zen/profiles.ini | sed 's/[Pp]ath=//')
+      #   fi
+
+      #   check_suffix=1
+      #   EOF
+      # '';
+    }))
   ];
 
   home-manager.users.${config.myConfig.username} = {
