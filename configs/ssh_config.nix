fa29ecaa0139b51864a7842396a21a82ff02e5c3
Connor Rhodes <connor@rhodes.contact>
2025-06-16 21:15:42 -0500

    update ssh config

--- configs/ssh_config.nix
+++ configs/ssh_config.nix
@@ -19,16 +19,16 @@
         SetEnv TERM=xterm-256color
 
       Host lat
-        Hostname ${secrets.ssh.config.mpro.ip}
-        Port ${secrets.ssh.config.mpro.port}
-        User ${secrets.ssh.config.mpro.user}
+        Hostname ${secrets.ssh.config.latitude.ip}
+        Port ${secrets.ssh.config.latitude.port}
+        User ${secrets.ssh.config.latitude.user}
         SetEnv TERM=xterm-256color
         ProxyJump m
 
       Host ph
-        Hostname ${secrets.ssh.config.mpro.ip}
-        User ${secrets.ssh.config.mpro.user}
-        Port ${secrets.ssh.config.mpro.port}
+        Hostname ${secrets.ssh.config.phone.ip}
+        User ${secrets.ssh.config.phone.user}
+        Port ${secrets.ssh.config.phone.port}
         SetEnv TERM=xterm-256color
     '';
   };
