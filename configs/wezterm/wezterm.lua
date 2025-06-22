655deb9ffe32ea120c24e648564351db4db29580
Connor Rhodes <connor@rhodes.contact>
2025-06-21 21:26:28 -0500

    update wezterm keybinds

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -31,10 +31,56 @@ config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.hide_mouse_cursor_when_typing = false
 
 config.keys = {
-  {
-    key = 'w',
+  { key = 'w',
     mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = false },
+    action = wezterm.action.CloseCurrentPane { confirm = false }, },
+  -- Pane splitting
+  {
+    key = 'h',
+    mods = 'CMD|OPT',
+    action = wezterm.action.SplitPane { direction = 'Left' },
+  },
+  {
+    key = 'j',
+    mods = 'CMD|OPT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'k',
+    mods = 'CMD|OPT',
+    action = wezterm.action.SplitPane { direction = 'Up' },
+  },
+  {
+    key = 'l',
+    mods = 'CMD|OPT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  -- Pane navigation
+  {
+    key = 'h',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'j',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'k',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'l',
+    mods = 'CTRL',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+  -- Clear terminal
+  {
+    key = ';',
+    mods = 'CTRL',
+    action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
   },
 }
 
