b9510a4f6cb3178195fa1c4746195aa1f10a627d
Connor Rhodes <connor@rhodes.contact>
2025-08-08 13:49:44 -0500

    update cursedtag

--- pkgs/cursedtag.nix
+++ pkgs/cursedtag.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
+# repo: https://github.com/hellricer/cursedtag
+# script: https://raw.githubusercontent.com/hellricer/cursedtag/refs/heads/master/cursedtag
+
 {
   # dependencies
   home.packages = with pkgs; [
