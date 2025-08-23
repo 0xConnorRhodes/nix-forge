3c21a5826a0459f3915cb633b0f6b6780a5ecf7d
Connor Rhodes <connor@rhodes.contact>
2025-08-23 13:43:23 -0500

    add mpv-lossless-cut

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -1,5 +1,16 @@
-{ inputs, config, ... }:
+{ inputs, config, pkgs, ... }:
 
+let
+  mpv-lossless-cut = pkgs.fetchFromGitHub rec {
+    # must make fetchFromGitHub recursive and bind name to rev to ensure that
+    # when rev is changed, the new version is fetched
+    name = "mpv-lossless-cut-${rev}";
+    owner = "f0e";
+    repo = "mpv-lossless-cut";
+    rev = "1f895d3";
+    sha256 = "sha256-pkrX7zbH6MQFe1971yJGhtpF2fabBrzOnL6S2P6tf34=";
+  };
+in
 {
   programs.mpv = {
     enable = true;
@@ -9,4 +20,7 @@
     save-position-on-quit=yes
     write-filename-in-watch-later-config=yes
   '';
+
+  # Install the lossless cut script
+  home.file.".config/mpv/scripts/mpv-lossless-cut.lua".source = "${mpv-lossless-cut}/mpv-lossless-cut.lua";
 }
