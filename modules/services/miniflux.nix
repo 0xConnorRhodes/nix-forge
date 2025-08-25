d38899223b3fc83c87b4257ff01e64bebde2779e
Connor Rhodes <connor@rhodes.contact>
2025-08-25 16:34:38 -0500

    comment miniflux module

--- modules/services/miniflux.nix
+++ modules/services/miniflux.nix
@@ -3,7 +3,7 @@
 {
   services.miniflux = {
     enable = true;
-    createDatabaseLocally = true;
+    createDatabaseLocally = true; # don't use external database
     # Provide dummy admin credentials file to satisfy NixOS module validation
     # even though CREATE_ADMIN is disabled
     adminCredentialsFile = pkgs.writeText "miniflux-dummy-admin-credentials" ''
@@ -17,12 +17,13 @@
       # Disable admin user creation explicitly
       CREATE_ADMIN = "0";
 
-      # Enable auth proxy authentication
-      AUTH_PROXY_USER_CREATION = "1";
-      AUTH_PROXY_HEADER = "Remote-User";  # Common auth proxy header
-
       # Disable local authentication since we're using proxy auth
       DISABLE_LOCAL_AUTH = "1";
+
+      # Enable auth proxy authentication
+      AUTH_PROXY_USER_CREATION = "1";
+      AUTH_PROXY_HEADER = "Remote-User";
+
     };
   };
 }
