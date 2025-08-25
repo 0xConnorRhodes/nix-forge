d017c938c0d0d14ec6835fbc372f6d39e33cd4af
Connor Rhodes <connor@rhodes.contact>
2025-08-25 16:33:48 -0500

    finish miniflux config

--- modules/services/miniflux.nix
+++ modules/services/miniflux.nix
@@ -4,14 +4,25 @@
   services.miniflux = {
     enable = true;
     createDatabaseLocally = true;
-    adminCredentialsFile = pkgs.writeText "miniflux-admin-credentials" ''
-      ADMIN_USERNAME=admin
-      ADMIN_PASSWORD=changeme123
+    # Provide dummy admin credentials file to satisfy NixOS module validation
+    # even though CREATE_ADMIN is disabled
+    adminCredentialsFile = pkgs.writeText "miniflux-dummy-admin-credentials" ''
+      ADMIN_USERNAME=unused
+      ADMIN_PASSWORD=unused
     '';
     config = {
       LISTEN_ADDR = "127.0.0.1:25279";
       BASE_URL = "http://127.0.0.1:25279";
-      CREATE_ADMIN = "1";
+
+      # Disable admin user creation explicitly
+      CREATE_ADMIN = "0";
+
+      # Enable auth proxy authentication
+      AUTH_PROXY_USER_CREATION = "1";
+      AUTH_PROXY_HEADER = "Remote-User";  # Common auth proxy header
+
+      # Disable local authentication since we're using proxy auth
+      DISABLE_LOCAL_AUTH = "1";
     };
   };
 }
