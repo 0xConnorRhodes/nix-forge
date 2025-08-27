c49ce68f1fe89d763b8c5ac1ff3c5d7dfc4a7cfb
Connor Rhodes <connor@rhodes.contact>
2025-08-26 22:14:41 -0500

    add media home.nix

---
+++ hosts/nixos/media/home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, inputs, ... }:
+let
+  pkgsUnstable = import inputs.nixpkgs-unstable {
+    inherit (pkgs.stdenv.hostPlatform) system;
+    inherit (config.nixpkgs) config;
+  };
+in
+{
+  # make pkgsUnstable available to all modules
+  _module.args.pkgsUnstable = pkgsUnstable;
+  imports = [
+    ../../common/home/git.nix
+    ../../common/home/bash.nix
+    ../../common/home/zsh.nix
+    ../../common/home/ripgrep.nix
+    ../../common/home/zoxide.nix
+    ../../common/home/starship.nix
+    ../../common/home/bat.nix
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "neovim";
+  };
+
+  programs.zsh = {
+    dirHashes = {
+      docs = "$HOME/Documents";
+    };
+    initContent = import ../../common/home/posixFunctions.nix;
+  };
+}
