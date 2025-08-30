0eec6c10e0ebeaef9a9983fe3f7272cb4a2f85e2
Connor Rhodes <connor@rhodes.contact>
2025-08-30 12:54:00 -0500

    udpate media packages

--- hosts/nixos/media/packages.nix
+++ hosts/nixos/media/packages.nix
@@ -26,10 +26,11 @@
     fd
     nh
     screen
-    mpv
-    vlc
+    mpv # fullscreen not working, likley same fix as vlc below
+    vlc # Tools=>Preferences=>Video - Then change Output from Automatic to OpenGL per https://forums.raspberrypi.com/viewtopic.php?t=337359 for fullscreen
     pipx
     libraspberrypi # pi diagonistic cli utilities
     pavucontrol
+    killall
   ];
 }
