5749bf1b6c690fb9898ff27727689af8fc193c45
Connor Rhodes <connor@rhodes.contact>
2025-08-31 20:02:53 -0500

    update nfs config for tailscale

--- hosts/nixos/mpro/nfs.nix
+++ hosts/nixos/mpro/nfs.nix
@@ -4,27 +4,29 @@
   # Enable NFS server
   services.nfs.server.enable = true;
 
-  # Configure exports
+  # Configure exports - restrict to Tailscale network
   services.nfs.server.exports = ''
-    /scary/share *(rw,sync,no_subtree_check,no_root_squash)
+    /scary/share 100.64.0.0/10(rw,sync,no_subtree_check,all_squash,anonuid=1000,anongid=100)
   '';
 
-  # Open firewall ports for NFS
-  networking.firewall.allowedTCPPorts = [
-    111   # portmapper
-    2049  # nfsd
-    4000  # lockd
-    4001  # mountd
-    4002  # statd
-  ];
+  # Open firewall ports for NFS only on Tailscale interface
+  networking.firewall.interfaces."tailscale0" = {
+    allowedTCPPorts = [
+      111   # portmapper
+      2049  # nfsd
+      4000  # lockd
+      4001  # mountd
+      4002  # statd
+    ];
 
-  networking.firewall.allowedUDPPorts = [
-    111   # portmapper
-    2049  # nfsd
-    4000  # lockd
-    4001  # mountd
-    4002  # statd
-  ];
+    allowedUDPPorts = [
+      111   # portmapper
+      2049  # nfsd
+      4000  # lockd
+      4001  # mountd
+      4002  # statd
+    ];
+  };
 
   # Ensure the directory exists and has proper permissions
   systemd.tmpfiles.rules = [
