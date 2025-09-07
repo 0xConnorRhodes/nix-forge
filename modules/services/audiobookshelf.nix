f94c44f9ea4f422d0016eff6033ee61891ca5c05
Connor Rhodes <connor@rhodes.contact>
2025-09-07 17:52:51 -0500

    bump abookshelf version

--- modules/services/audiobookshelf.nix
+++ modules/services/audiobookshelf.nix
@@ -1,8 +1,9 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, pkgsUnstable, ... }:
 
 {
   services.audiobookshelf = {
     enable = true;
+    package = pkgsUnstable.audiobookshelf;
     host = "127.0.0.1";
     port = 61793;
   };
