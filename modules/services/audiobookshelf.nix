8407703b669204e49979ac050027fba0c52299ed
Connor Rhodes <connor@rhodes.contact>
2025-09-16 18:47:51 -0500

    enable audiobookshelf backup

--- modules/services/audiobookshelf.nix
+++ modules/services/audiobookshelf.nix
@@ -10,56 +10,56 @@
 
 
   # APP STATE BACKUP
-  # systemd.timers."audiobookshelf-db-backup" = {
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     OnCalendar = "daily";
-  #     Persistent = true;
-  #     Unit = "audiobookshelf-db-backup.service";
-  #   };
-  # };
+  systemd.timers."audiobookshelf-db-backup" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      Unit = "audiobookshelf-db-backup.service";
+    };
+  };
 
-  # systemd.services."audiobookshelf-db-backup" = {
-  #   path =  with pkgs; [
-  #     bash
-  #     coreutils
-  #     findutils
-  #     gnugrep
-  #     rclone
-  #   ];
-  #   script = ''
-  #     set -eu
-  #     #!/bin/bash
+  systemd.services."audiobookshelf-db-backup" = {
+    path =  with pkgs; [
+      bash
+      coreutils
+      findutils
+      gnugrep
+      rclone
+    ];
+    script = ''
+      set -eu
+      #!/bin/bash
 
-  #     SOURCE_DIR="/var/lib/audiobookshelf/metadata/backups"
-  #     DEST_DIR="/mnt/zdata/records/db_backups/audiobookshelf"
+      SOURCE_DIR="/var/lib/audiobookshelf/metadata/backups"
+      DEST_DIR="/zstore/data/records/db_backups/audiobookshelf"
 
-  #     echo "Checking existing files in destination..."
-  #     EXISTING_FILES=$(find "$DEST_DIR" -maxdepth 1 -type f -printf "%f\n" 2>/dev/null || true)
+      echo "Checking existing files in destination..."
+      EXISTING_FILES=$(find "$DEST_DIR" -maxdepth 1 -type f -printf "%f\n" 2>/dev/null || true)
 
-  #     find "$SOURCE_DIR" -maxdepth 1 -type f | while read -r SOURCE_FILE; do
-  #       FILENAME=$(basename "$SOURCE_FILE")
+      find "$SOURCE_DIR" -maxdepth 1 -type f | while read -r SOURCE_FILE; do
+        FILENAME=$(basename "$SOURCE_FILE")
 
-  #       if echo "$EXISTING_FILES" | grep -Fxq "$FILENAME"; then
-  #         echo "Skipping $FILENAME (already exists in destination)"
-  #       else
-  #         echo "Copying $FILENAME..."
-  #         cp "$SOURCE_FILE" "$DEST_DIR/"
-  #         if [ $? -eq 0 ]; then
-  #           echo "Successfully copied $FILENAME"
-  #         else
-  #           echo "Error copying $FILENAME"
-  #         fi
-  #       fi
-  #     done
+        if echo "$EXISTING_FILES" | grep -Fxq "$FILENAME"; then
+          echo "Skipping $FILENAME (already exists in destination)"
+        else
+          echo "Copying $FILENAME..."
+          cp "$SOURCE_FILE" "$DEST_DIR/"
+          if [ $? -eq 0 ]; then
+            echo "Successfully copied $FILENAME"
+          else
+            echo "Error copying $FILENAME"
+          fi
+        fi
+      done
 
-  #     rclone copy "$SOURCE_DIR" dropbox_enc:db_backups/audiobookshelf --ignore-existing
+      rclone copy "$SOURCE_DIR" dropbox_enc:db_backups/audiobookshelf --ignore-existing
 
-  #     echo "Backup operation completed."
-  #   '';
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     User = "${config.myConfig.username}";
-  #   };
-  # };
+      echo "Backup operation completed."
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "${config.myConfig.username}";
+    };
+  };
 }
