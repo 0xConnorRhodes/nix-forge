245eae4bda414c5aa4863307873afdd11a9d7c2e
Connor Rhodes <connor@rhodes.contact>
2025-09-27 17:21:16 -0500

    add immich backup service

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -1,13 +1,13 @@
 { config, lib, pkgs, pkgsUnstable, ... }:
 
 {
-  services.immich = {
-    enable = true;
-    package = pkgsUnstable.immich;
-    host = "127.0.0.1";
-    port = 4312;
-    user = config.myConfig.username;
-  };
+  # services.immich = {
+  #   enable = true;
+  #   package = pkgsUnstable.immich;
+  #   host = "127.0.0.1";
+  #   port = 4312;
+  #   user = config.myConfig.username;
+  # };
 
   systemd.timers."immich-backup-docker" = {
     wantedBy = [ "timers.target" ];
@@ -21,11 +21,22 @@
   systemd.services."immich-backup-docker" = {
     path = with pkgs; [
       docker
+      gnutar
+      gzip
+      coreutils
+      rclone
     ];
     script = ''
       set -e
       PATH="/run/wrappers/bin:$PATH"
+      BACKUP_FILE="$(date +%y%m%d)-immich-db-backup.tar.gz"
+
       docker compose -f /home/connor/code/infra/immich/compose.yml down
+      sudo tar -czpf /tmp/$BACKUP_FILE -C /home/connor/.local/docker_data immich
+      sudo chown connor:users /tmp/$BACKUP_FILE
+      mv /tmp/$BACKUP_FILE /zstore/data/records/db_backups/immich
+      rclone copy /zstore/data/records/db_backups/immich dropbox_enc:db_backups/immich --ignore-existing
+      docker compose -f /home/connor/code/infra/immich/compose.yml up -d
     '';
     serviceConfig = {
       Type = "oneshot";
