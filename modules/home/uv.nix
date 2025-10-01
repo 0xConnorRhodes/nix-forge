95e6aacf07edd10e1384e03f0105ba4844efceed
Connor Rhodes <connor@rhodes.contact>
2025-10-01 15:11:46 -0500

    set uv python interpreter path

--- modules/home/uv.nix
+++ modules/home/uv.nix
@@ -8,4 +8,9 @@
       python-preference = "only-system";   # only use system Python
     };
   };
+
+  # set python interpreter path for uv
+  home.sessionVariables = {
+    UV_PYTHON = "${pkgs.python3}/bin/python3";
+  };
 }
