59997aae7afb6fc4cb5ec9ae08415b253670b90d
Connor Rhodes <connor@rhodes.contact>
2025-10-02 20:36:25 -0500

    initial linux modkeys

--- configs/wezterm/host_cfg.lua
+++ configs/wezterm/host_cfg.lua
@@ -9,21 +9,25 @@ if hostname == 'mpro' then
   host_cfg.modAlt = 'CTRL'
   host_cfg.modWin = 'WIN'
   host_cfg.modCtrl = 'ALT'
+  host_cfg.remap_alt_to_ctrl = true  -- Enable Alt_L to Control_L remapping
 elseif hostname == 'latitude' then
   host_cfg.font_size = 17
   host_cfg.modAlt = 'CTRL'
   host_cfg.modWin = 'WIN'
   host_cfg.modCtrl = 'ALT'
+  host_cfg.remap_alt_to_ctrl = true  -- Enable Alt_L to Control_L remapping
 elseif wezterm.target_triple:find("darwin") then
   host_cfg.font_size = 21
   host_cfg.modAlt = 'CMD'
   host_cfg.modWin = 'OPT'
   host_cfg.modCtrl = 'CTRL'
+  host_cfg.remap_alt_to_ctrl = false  -- Disable Alt_L to Control_L remapping on macOS
 else
   host_cfg.font_size = 21
   host_cfg.modAlt = 'ALT'
   host_cfg.modWin = 'WIN'
   host_cfg.modCtrl = 'CTRL'
+  host_cfg.remap_alt_to_ctrl = false  -- Disable Alt_L to Control_L remapping by default
 end
 
 return host_cfg
--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -16,6 +16,47 @@ config.font = wezterm.font 'GeistMono Nerd Font'
 
 config.font_size = host_cfg.font_size
 
+-- Key remapping: Alt_L to Control_L based on host configuration
+if host_cfg.remap_alt_to_ctrl then
+  config.key_map_preference = "Physical"
+  config.send_composed_key_when_left_alt_is_pressed = false
+  config.send_composed_key_when_right_alt_is_pressed = true
+
+  -- Map physical Alt_L to logical Control_L
+  config.key_tables = {
+    copy_mode = {},
+    search_mode = {},
+  }
+
+  -- Add key mappings to remap Alt_L to Control_L
+  local alt_to_ctrl_keys = {}
+
+  -- Common control key combinations that should be remapped
+  local control_keys = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    '-', '=', '[', ']', '\\', ';', "'", ',', '.', '/',
+    'Space', 'Tab', 'Enter', 'Escape', 'Backspace', 'Delete',
+    'Home', 'End', 'PageUp', 'PageDown',
+    'LeftArrow', 'RightArrow', 'UpArrow', 'DownArrow',
+    'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'
+  }
+
+  for _, key in ipairs(control_keys) do
+    table.insert(alt_to_ctrl_keys, {
+      key = key,
+      mods = 'ALT',
+      action = wezterm.action.SendKey { key = key, mods = 'CTRL' }
+    })
+  end
+
+  -- Merge the remapping keys with existing keybinds
+  for _, key_mapping in ipairs(alt_to_ctrl_keys) do
+    table.insert(keybinds.keys, key_mapping)
+  end
+end
+
 config.window_padding = {
   left = 0,
   right = 0,
