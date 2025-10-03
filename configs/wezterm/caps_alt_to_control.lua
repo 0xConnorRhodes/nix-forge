b1e28477b2afac18ccf354a7d01361c9d43bffe0
Connor Rhodes <connor@rhodes.contact>
2025-10-02 20:49:17 -0500

    update wezterm

---
+++ configs/wezterm/caps_alt_to_control.lua
@@ -0,0 +1,53 @@
+local wezterm = require('wezterm')
+
+local M = {}
+
+-- Function to set up Alt_L to Control_L remapping based on host configuration
+function M.setup_alt_to_ctrl_remapping(config, host_cfg, keybinds)
+  if not host_cfg.remap_alt_to_ctrl then
+    return
+  end
+
+  config.key_map_preference = "Physical"
+  config.send_composed_key_when_left_alt_is_pressed = false
+  config.send_composed_key_when_right_alt_is_pressed = true
+
+  -- Map physical Alt_L to logical Control_L
+  config.key_tables = {
+    copy_mode = {},
+    search_mode = {},
+  }
+
+  -- Add key mappings to remap Alt_L to Control_L
+  local alt_to_ctrl_keys = {}
+
+  -- Alt key combinations that should be remapped
+  -- local control_keys = {
+  --   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  --   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  --   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+  --   '-', '=', '[', ']', '\\', ';', "'", ',', '.', '/',
+  --   'Space', 'Tab', 'Enter', 'Escape', 'Backspace', 'Delete',
+  --   'Home', 'End', 'PageUp', 'PageDown',
+  --   'LeftArrow', 'RightArrow', 'UpArrow', 'DownArrow',
+  --   'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'
+  -- }
+  local control_keys = {
+    'c', 'k',
+  }
+
+  for _, key in ipairs(control_keys) do
+    table.insert(alt_to_ctrl_keys, {
+      key = key,
+      mods = 'ALT',
+      action = wezterm.action.SendKey { key = key, mods = 'CTRL' }
+    })
+  end
+
+  -- Merge the remapping keys with existing keybinds
+  for _, key_mapping in ipairs(alt_to_ctrl_keys) do
+    table.insert(keybinds.keys, key_mapping)
+  end
+end
+
+return M
--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require('wezterm')
 local host_cfg = require('host_cfg')
 local keybinds = require('keybinds')
+local caps_alt_to_control = require('caps_alt_to_control')
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Abernathy'
@@ -17,45 +18,7 @@ config.font = wezterm.font 'GeistMono Nerd Font'
 config.font_size = host_cfg.font_size
 
 -- Key remapping: Alt_L to Control_L based on host configuration
-if host_cfg.remap_alt_to_ctrl then
-  config.key_map_preference = "Physical"
-  config.send_composed_key_when_left_alt_is_pressed = false
-  config.send_composed_key_when_right_alt_is_pressed = true
-
-  -- Map physical Alt_L to logical Control_L
-  config.key_tables = {
-    copy_mode = {},
-    search_mode = {},
-  }
-
-  -- Add key mappings to remap Alt_L to Control_L
-  local alt_to_ctrl_keys = {}
-
-  -- Alt key combinations that should be remapped
-  local control_keys = {
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    '-', '=', '[', ']', '\\', ';', "'", ',', '.', '/',
-    'Space', 'Tab', 'Enter', 'Escape', 'Backspace', 'Delete',
-    'Home', 'End', 'PageUp', 'PageDown',
-    'LeftArrow', 'RightArrow', 'UpArrow', 'DownArrow',
-    'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12'
-  }
-
-  for _, key in ipairs(control_keys) do
-    table.insert(alt_to_ctrl_keys, {
-      key = key,
-      mods = 'ALT',
-      action = wezterm.action.SendKey { key = key, mods = 'CTRL' }
-    })
-  end
-
-  -- Merge the remapping keys with existing keybinds
-  for _, key_mapping in ipairs(alt_to_ctrl_keys) do
-    table.insert(keybinds.keys, key_mapping)
-  end
-end
+caps_alt_to_control.setup_alt_to_ctrl_remapping(config, host_cfg, keybinds)
 
 config.window_padding = {
   left = 0,
