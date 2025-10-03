0b23652a593374a286907f483ac4b30dd60ebc07
Connor Rhodes <connor@rhodes.contact>
2025-10-02 20:51:49 -0500

    add symlink to new lua module

--- configs/wezterm/wezterm-common.nix
+++ configs/wezterm/wezterm-common.nix
@@ -4,4 +4,5 @@
   home.file.".config/wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink ./wezterm.lua;
   home.file.".config/wezterm/keybinds.lua".source = config.lib.file.mkOutOfStoreSymlink ./keybinds.lua;
   home.file.".config/wezterm/host_cfg.lua".source = config.lib.file.mkOutOfStoreSymlink ./host_cfg.lua;
+  home.file.".config/wezterm/caps_alt_to_control.lua".source = config.lib.file.mkOutOfStoreSymlink ./caps_alt_to_control.lua;
 }
