ebe68d9f7698ef6c8e2933ec3de54a2e3ee96628
Connor Rhodes <connor@rhodes.contact>
2025-10-05 13:20:11 -0500

    add uv shell completions

--- modules/home/uv.nix
+++ modules/home/uv.nix
@@ -1,5 +1,14 @@
 { inputs, config, pkgs, ... }:
 
+let
+  # Generate uv completions once at build time
+  uvCompletions = pkgs.runCommand "uv-completions" {
+    buildInputs = [ pkgs.uv ];
+  } ''
+    mkdir -p $out/share/zsh/site-functions
+    ${pkgs.uv}/bin/uv generate-shell-completion zsh > $out/share/zsh/site-functions/_uv
+  '';
+in
 {
   programs.uv = {
     enable = true;
@@ -12,4 +21,9 @@
   # set global default python version
   # equivalent to uv python pin --global <version>
   home.file.".config/uv/.python-version".text = "3.12";
+
+  # Add uv shell completions to zsh by sourcing pre-generated completion file
+  programs.zsh.initContent = ''
+    source ${uvCompletions}/share/zsh/site-functions/_uv
+  '';
 }
