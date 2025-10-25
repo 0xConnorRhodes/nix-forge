55d86c3a31f7b761259ddb0cb170e44beef537c6
Connor Rhodes <connor@rhodes.contact>
2025-10-24 21:10:26 -0500

    update amvim

--- modules/home/vscode/amvim.nix
+++ modules/home/vscode/amvim.nix
@@ -13,6 +13,16 @@
       when = "editorTextFocus && amVim.mode == 'NORMAL' && !amVim.waitingForInput";
     }
     {
+      key = "w";
+      command = "cursorWordRight";
+      when = "editorTextFocus && amVim.mode == 'NORMAL' && !amVim.waitingForInput";
+    }
+    {
+      key = "b";
+      command = "cursorWordLeft";
+      when = "editorTextFocus && amVim.mode == 'NORMAL' && !amVim.waitingForInput";
+    }
+    {
       key = "k";
       command = "cursorUp";
       when = "editorTextFocus && amVim.mode == 'NORMAL' && !amVim.waitingForInput";
@@ -20,6 +30,20 @@
     {
       key = "enter";
       command = "editor.toggleFold";
+      when = "editorTextFocus && amVim.mode == 'NORMAL' && !amVim.waitingForInput";
+    }
+    {
+      # note, the cursorUndo command only works as expected if you pause before Y
+      # there appears to be a timeout before vscode registors the cursor position
+      key = "shift+y";
+      command = "runCommands";
+      args = {
+        commands = [
+          "cursorEndSelect"
+          "editor.action.clipboardCopyAction"
+          "cursorUndo"
+        ];
+      };
       when = "editorTextFocus && amVim.mode == 'NORMAL' && !amVim.waitingForInput";
     }
   ];
