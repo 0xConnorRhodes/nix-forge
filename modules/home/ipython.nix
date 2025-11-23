c0e2ee8489334f5919554ec8888fa7f73274ca72
Connor Rhodes <connor@rhodes.contact>
2025-11-22 21:16:15 -0600

    update ipython

--- modules/home/ipython.nix
+++ modules/home/ipython.nix
@@ -4,5 +4,6 @@
   home.file.".ipython/profile_default/ipython_config.py".text = ''
     c = get_config()  #noqa
     c.TerminalInteractiveShell.confirm_exit = False
+    c.TerminalInteractiveShell.colors = 'Neutral'
   '';
 }
