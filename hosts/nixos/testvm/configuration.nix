d3d80873744d8c60f89d3a457e42e062f22e39cb
Connor Rhodes <connor@rhodes.contact>
2025-12-02 17:30:13 -0600

    prune testvm config

--- hosts/nixos/testvm/configuration.nix
+++ hosts/nixos/testvm/configuration.nix
@@ -4,24 +4,11 @@
   imports = [
     ./hardware-configuration.nix
     ./packages.nix
-    ./secret.nix
     ../../common/host-options.nix
     ../../common/nixos-common.nix
-    inputs.home-manager.nixosModules.default
-    inputs.nix-index-database.nixosModules.nix-index
   ];
 
   config = {
-    myConfig = {
-      hostname = "testvm";
-      username = "connor";
-      homeDir = "/home/connor";
-      trashcli = "trash";
-      modAlt = "ctrl";
-      modCtrl = "alt";
-      hostPaths = [];
-    };
-
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
@@ -32,15 +19,9 @@
       isNormalUser = true;
       description = "Connor Rhodes";
       extraGroups = [ "networkmanager" "wheel" ];
-      shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFRNR1Ee12nbxJcXbzhq6WUkkGn8mADvtRERAs3ufLOK"
       ];
-    };
-
-    home-manager.users.${config.myConfig.username} = { pkgs, ... }: {
-      home.stateVersion = "25.11";
-      imports = [ ./home.nix ];
     };
 
     system.stateVersion = "25.11";
--- hosts/nixos/testvm/home.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, inputs, secrets, ... }:
-
-{
-  # Basic home manager configuration
-  programs.home-manager.enable = true;
-  programs.zsh.enable = true;
-
-  home.stateVersion = "25.11";
-}
