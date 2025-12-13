bb334b4b9fee2668e9c1919201350b79afcf7ade
Connor Rhodes <connor@rhodes.contact>
2025-12-12 19:48:19 -0600

    add authelia instance for connor.engineer

--- modules/nixos/authelia.nix
+++ modules/nixos/authelia.nix
@@ -3,12 +3,25 @@
 {
   environment.systemPackages = [ pkgs.authelia ];
 
-  systemd.services.authelia = {
+  # First Authelia instance (connorrhodes.com)
+  systemd.services.authelia-connorrhodes-com = {
     enable = true;
     after = [ "redis-authelia.service" ];
     wants = [ "redis-authelia.service" ];
     serviceConfig = {
-      ExecStart = "${pkgs.authelia}/bin/authelia --config /home/connor/code/infra/authelia/authelia-conf.yml";
+      ExecStart = "${pkgs.authelia}/bin/authelia --config /home/connor/code/infra/authelia/conf-connorrhodes-com.yml";
+      Restart = "always";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  # Second Authelia instance (connor-engineer)
+  systemd.services.authelia-connor-engineer = {
+    enable = true;
+    after = [ "redis-authelia.service" ];
+    wants = [ "redis-authelia.service" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.authelia}/bin/authelia --config /home/connor/code/infra/authelia/conf-connor-engineer.yml";
       Restart = "always";
     };
     wantedBy = [ "multi-user.target" ];
