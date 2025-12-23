8e64611e9b8fe0227f07ad2ad594244c590fa80d
Connor Rhodes <connor@rhodes.contact>
2025-12-23 15:11:49 -0600

    comment openwebui

--- modules/services/open-webui.nix
+++ modules/services/open-webui.nix
@@ -6,6 +6,7 @@
     host = "127.0.0.1";
     port = 58139;
     environment = {
+      # set nonsense prompt suggestions to hide element
       DEFAULT_PROMPT_SUGGESTIONS = builtins.toJSON [
         {
           title = [
