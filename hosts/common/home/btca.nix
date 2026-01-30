dc4a92e1bab2a52fbdfe4c75f64f59567f1d2c75
Connor Rhodes <connor@rhodes.contact>
2026-01-30 17:38:50 -0600

    add vikunja api clint to btca

--- hosts/common/home/btca.nix
+++ hosts/common/home/btca.nix
@@ -7,13 +7,26 @@
     provider = secrets.btca.provider;
     providerTimeoutMs = 300000;
     resources = [
+      # {
+      #   name = "better-context";
+      #   type = "git";
+      #   url = "https://github.com/davis7dotsh/better-context";
+      #   branch = "main";
+      #   # searchPath = "apps/svelte.dev";
+      #   # searchPaths = ["path/one" "path/two"];
+      #   # specialNotes = "Focus on the content directory for docs";
+      # }
       {
-        type = "git";
         name = "better-context";
+        type = "git";
         url = "https://github.com/davis7dotsh/better-context";
         branch = "main";
-        # searchPath = "apps/svelte.dev";
-        # specialNotes = "Focus on the content directory for docs";
+      }
+      {
+        name = "pyvikunja";
+        type = "local";
+        path = "${config.home.homeDirectory}/code/vikunja-dev";
+        searchPath = "module/vikunja";
       }
     ];
   };
