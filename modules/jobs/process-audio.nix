7367df95cf3dbb0aa0e27d3a82eeba8c1debe23a
Connor Rhodes <connor@rhodes.contact>
2026-04-25 11:43:52 -0500

    update transcode-music job

--- modules/jobs/process-audio.nix
+++ modules/jobs/process-audio.nix
@@ -5,22 +5,20 @@ let
 in
 {
 # transcode music library
-  systemd.timers."transcode-musiclibrary" = {
+  systemd.timers."transcode-music" = {
     wantedBy = [ "timers.target" ];
       timerConfig = {
         OnCalendar = "02:00:00";
         Persistent = true;
-        Unit = "transcode-musiclibrary.service"; }; };
-  systemd.services."transcode-musiclibrary" = {
+        Unit = "transcode-music.service"; }; };
+  systemd.services."transcode-music" = {
     script = ''
       set -eu
-      export PATH="${pkgs.uv}/bin:/run/current-system/sw/bin:$PATH"
-      /home/${user}/code/scripts/cron/transcode-musiclibrary
+      /home/${user}/.local/bin/transcode-music
     '';
     serviceConfig = {
       Type = "oneshot";
-      User = user;
-      WorkingDirectory = "/home/${user}/code/scripts/cron"; }; };
+      User = user; }; };
 
 # auto process audio
   systemd.timers."auto-process-audio" = {
