e0573b3b8a6f01591409508261ba7e52ce267e39
Connor Rhodes <connor@rhodes.contact>
2026-05-09 18:43:50 -0500

    update silverbullet service for sb ignore support

--- modules/services/silverbullet.nix
+++ modules/services/silverbullet.nix
@@ -1,16 +1,27 @@
-{ config, pkgs, pkgsUnstable, ... }:
+{ config, pkgs, pkgsUnstable, lib, ... }:
 
+let
+  ignorePatterns = lib.concatStringsSep "\\n" [
+    "skills"
+    # "folder2"
+  ];
+
+  # this indirection is needed to ensure proper syntax in generated command
+  # export SB_SPACE_IGNORE=$'folder1\nfolder2'
+  silverbulletWithIgnore = pkgs.writeShellScriptBin "silverbullet" (
+    "export SB_SPACE_IGNORE=$'" + ignorePatterns + "'\n"
+    + "exec ${lib.getExe pkgs.silverbullet} \"$@\""
+  );
+in
 {
   services.silverbullet = {
     enable = true;
-    package = pkgs.silverbullet;
+    package = silverbulletWithIgnore;
     user = config.myConfig.username;
     group = "users";
     spaceDir = "${config.myConfig.homeDir}/code/notes";
     listenAddress = "127.0.0.1";
     listenPort = 38157;
   };
-
-  systemd.services.silverbullet.serviceConfig.Environment =
-    [ "SB_SPACE_IGNORE=Inbox/voice_notes" ];
 }
+
